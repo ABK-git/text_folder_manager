c587410b67bdb142b7a6226a2c90aa339c669ec0
ABK-git <62530997+ABK-git@users.noreply.github.com>
2021-01-03 21:01:19 +0900

    Revert "Revert "個人データ読み込みの準備""

--- resources/js/components/folder-and-text-buttons/folder-and-text-buttons.component.jsx
+++ resources/js/components/folder-and-text-buttons/folder-and-text-buttons.component.jsx
@@ -1,71 +1,16 @@
 import React, { useState } from "react";
-import { connect } from "react-redux";
-import { createFolder } from "../../redux/folder/folder.actions";
+//component
+import CreateFolderForm from "../create-folder-form/create-folder-form.component";
 import CustomButton from "../custom-button/custom-button.component";
-import FormInput from "../form-input/form-input.component";
 
-import {
-    CreateFolderAndTextContainer,
-    FolderDiv,
-    FormAndButton
-} from "./folder-and-text-buttons.styles.jsx";
+import { CreateFolderAndTextContainer } from "./folder-and-text-buttons.styles.jsx";
 
-const FolderAndTextButtons = ({createFolder}) => {
-    const [isDisplay, setIsDisplay] = useState(false);
+const FolderAndTextButtons = () => (
 
-    const handleClick = () => {
-        setIsDisplay(!isDisplay);
-    };
+    <CreateFolderAndTextContainer>
+        <CustomButton design="createText">Text</CustomButton>
+        <CreateFolderForm />
+    </CreateFolderAndTextContainer>
+);
 
-    const [folderCredentials, setFolderCredentials] = useState({
-        folder_name: ""
-    });
-
-    const handleChange = event => {
-        const { value, name } = event.target;
-
-        setFolderCredentials({ ...folderCredentials, [name]: value });
-
-        console.log(folderCredentials.folder_name);
-    };
-
-    const handleSubmit = event => {
-        event.preventDefault();
-        console.log("handleSubmit");
-
-        createFolder(folderCredentials);
-    };
-
-    return (
-        <CreateFolderAndTextContainer>
-            <CustomButton design="createText">Text</CustomButton>
-            <FolderDiv>
-                <CustomButton design="createFolder" onClick={handleClick}>
-                    Folder
-                </CustomButton>
-
-                <FormAndButton
-                    onSubmit={handleSubmit}
-                    style={{ display: isDisplay ? "" : "none" }}
-                >
-                    <FormInput
-                        type="text"
-                        name="folder_name"
-                        handleChange={handleChange}
-                        required
-                    ></FormInput>
-                    <CustomButton type="text" design="createFolderSubmit">
-                        作成
-                    </CustomButton>
-                </FormAndButton>
-            </FolderDiv>
-        </CreateFolderAndTextContainer>
-    );
-};
-
-const mapDispatchToProps = dispatch => ({
-    createFolderStart: folderCredentials =>
-        dispatch(createFolderStart(folderCredentials))
-});
-
-export default connect(null, mapDispatchToProps)(FolderAndTextButtons);
+export default FolderAndTextButtons;
