3bc354cd1d7c54c22b539372e0b8e79e54c28b8d
ABK-git <62530997+ABK-git@users.noreply.github.com>
2021-01-16 19:19:48 +0900

    文字列をform入力欄の入力値で変更できるようにした

--- resources/js/pages/test/test_component.styles.jsx
+++ resources/js/pages/test/test_component.styles.jsx
@@ -1,11 +1,23 @@
-import styled, {css} from "styled-components";
+import styled, { css } from "styled-components";
 
 export const ChangeString = styled.span`
-    font-size: 24px;
-    color: #FA2917;
+    font-size: 20px;
+    color: #fa2917;
 `;
 
 export const LineDiv = styled.div`
     height: 20px;
     font-size: 16px;
+`;
+
+//文章表示
+export const DisplayText = styled.div`
+    height: 100%;
+    width: 70%;
+`;
+//入力欄表示
+export const DisplayForm = styled.div`
+    height: 100%;
+    width: 30%;
+    text-align: center;
 `;
