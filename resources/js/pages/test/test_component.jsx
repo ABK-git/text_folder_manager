2d20dc056bbacdb0af2530b4d97cc3cdf164173a
ABK-git <62530997+ABK-git@users.noreply.github.com>
2021-01-16 18:27:48 +0900

    {}で囲まれた部分をタグに囲んで置き換える

--- resources/js/pages/test/test_component.jsx
+++ resources/js/pages/test/test_component.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+//component
+import Line from "../../components/line/line.component";
 
 const TestPage = () => {
     //locationを取得
     const location = useLocation();
     //行ごとに分割する
     const splitLine = location.state.creating_text.split("\n");
-    console.log(splitLine);
 
     return (
         <div>
@@ -14,10 +15,10 @@ const TestPage = () => {
                 //文字がない行の場合
                 if (line === "") {
                     //改行だけする
-                    return <br key={index}/>;
+                    return <br key={index} />;
                 }
 
-                return <p key={index}>{line}</p>;
+                return <Line key={index}>{line}</Line>
             })}
         </div>
     );
---
+++ resources/js/pages/test/test_component.styles.jsx
@@ -0,0 +1,11 @@
+import styled, {css} from "styled-components";
+
+export const ChangeString = css`
+    font-size: 24px;
+    color: #FA2917;
+`;
+
+export const LineDiv = styled.div`
+    height: 20px;
+    font-size: 16px;
+`;
